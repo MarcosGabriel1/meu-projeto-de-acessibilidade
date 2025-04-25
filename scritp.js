72336bf44aa57a6594c74c56f730c2f8ae83159d
MarcosGabriel1 <marcos.barbosa.almeida@escola.pr.gov.br>
2025-04-25 08:04:07 -0300

    Update scritp.js

--- scritp.js
+++ scritp.js
@@ -1,19 +1,1 @@
-// Função para aumentar o tamanho da fonte
-function aumentarFonte() {
-    let body = document.body;
-    let currentSize = window.getComputedStyle(body).fontSize;
-    let newSize = parseFloat(currentSize) * 1.1;  // Aumenta a fonte em 10%
-    body.style.fontSize = newSize + 'px';
-}
 
-// Função para diminuir o tamanho da fonte
-function diminuirFonte() {
-    let body = document.body;
-    let currentSize = window.getComputedStyle(body).fontSize;
-    let newSize = parseFloat(currentSize) * 0.9;  // Diminui a fonte em 10%
-    body.style.fontSize = newSize + 'px';
-}
-
-// Atribuir os eventos aos botões de aumentar e diminuir a fonte
-document.getElementById('aumentar-fonte').addEventListener('click', aumentarFonte);
-document.getElementById('diminuir-fonte').addEventListener('click', diminuirFonte);
